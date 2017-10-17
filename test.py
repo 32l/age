9ea45632d0fd8356b58f376a43873cdbc0b2d12c
ly015 <liyining0712@gmail.com>
2017-10-17 09:46:52 +0800

    Test windows github

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
 import argparse
 
-ps_1 = argparse.ArgumentParser(add_help = False)
-ps_1.add_argument('--arg1', type = int, default = 1)
-
-ps_2 = argparse.ArgumentParser(parents = [ps_1])
-ps_2.add_argument('--arg2', type = int, default = 2)
-
-
-args = ps_2.parse_args()
-
-print(args)
+print('Test github windows')
